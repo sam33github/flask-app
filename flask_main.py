7a3c0650f2c843253b5e3c4b01aeebc4cc2c5cb3
sam <sammuthuraj33@gmail.com>
2023-07-01 23:15:21 -0700

    sampython

--- flask_main.py
+++ flask_main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request, redirect
 import json_utilies
 import register
 import update
@@ -11,6 +11,33 @@ def flask():
         register.register(request.form["books"],request.form["courses"])
     data=json_utilies.student("data/student.json")
     return render_template("main.html",course1=data["students"])
+
+@app.route("/update/<int:id>",methods=["POST","GET"])
+def update_stud(id):
+    if request.method=="GET":
+        data=json_utilies.student("data/student.json")
+        stud_data={}
+        for stud in data['students']:
+            if stud["sno"]==int(id):
+                stud_data=stud
+        print(stud_data)
+        return render_template("update.html",stud_data=stud_data)
+    elif request.method=="POST":
+        user_name=request.form["stud_name"]
+        course=request.form["stud_course"]
+        status=request.form["stud_status"]
+        data=json_utilies.student("data/student.json")
+        for stud in data['students']:
+            if stud["sno"]==int(id):
+                stud["student_name"]=user_name
+                stud["student_course"]=course
+                stud["student_status"]=status
+        json_utilies.write("data/student.json",data)
+        return redirect("/")
+
+@app.route("/delete/<int:id>",methods=["POST","GET"])
+def delete_stud(id):
+    pass
 
     
 if __name__=="__main__":
